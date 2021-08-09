a4d87da9dbefc89dfec28306f195b1bc8a1b0650
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-09 15:42:32 +0530

    Completed chcek Employee present UC

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,5 +1,15 @@
 public class EmployeeWageComputation {
+    public static final int IS_PRESENT=1;
+
     public static void main(String[] args) {
-        
+        double empcheck=Math.floor(Math.random()*10)%2;
+        if(empcheck==IS_PRESENT)
+        {
+            System.out.println("The Employee is Present");
+        }
+        else
+        {
+            System.out.println("The Employee is Absent");
+        }
     }
 }
