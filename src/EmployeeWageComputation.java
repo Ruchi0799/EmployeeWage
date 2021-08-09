359ebf074e235d539436bf13c4d9bacff43b9104
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-09 16:48:04 +0530

    Added cal montlywage till max hour or days

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,36 +1,41 @@
-import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
 
 public class EmployeeWageComputation {
-    public static final int IS_PRESENT=2;
-    public static final int IS_PRESENT_PARTTIME=1;
-    public static final int WAGE_PER_HOUR=20;
+    public static final int IS_PRESENT = 2;
+    public static final int IS_PRESENT_PARTTIME = 1;
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int MAX_DAYS = 20;
+    public static final int MAX_HOURS = 100;
 
     public static void main(String[] args) {
-        int emphours=0;
-        int empwage=0;
-        double totalempwage=0;
+        int emphours = 0;
+        int empwage = 0;
+        //double totalempwage = 0;
+        int totalhours = 0;
+        int totalworkingday=0;
 
-        for (int i=1;i<=20;i++)
+        while (totalhours <= MAX_HOURS || totalworkingday<=MAX_DAYS)
         {
-            double empcheck=Math.floor(Math.random()*10)%3;
-
-            if(empcheck==IS_PRESENT)
+            totalworkingday++;
+            double empcheck = Math.floor(Math.random() * 10) % 3;
+            if (empcheck == IS_PRESENT)
             {
-                emphours=8;
+                emphours = 8;
             }
-            else if (empcheck==IS_PRESENT_PARTTIME)
+            else if (empcheck == IS_PRESENT_PARTTIME)
             {
-                emphours=4;
+                emphours = 4;
             }
             else
             {
-                emphours=0;
+                emphours = 0;
             }
-            empwage=WAGE_PER_HOUR*emphours;
-            totalempwage=totalempwage+empwage;
+            totalhours = totalhours + emphours;
+
 
         }
-        System.out.println("Employee Monthly wage =" + totalempwage);
+        empwage = WAGE_PER_HOUR * totalhours;
 
+        System.out.println("Employee Monthly wage =" + empwage);
     }
 }
