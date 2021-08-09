710ce603c8be5ca1d28dc64a71f88a60b1c99745
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-09 15:50:14 +0530

    Completed calculate daily wages

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,15 +1,16 @@
 public class EmployeeWageComputation {
     public static final int IS_PRESENT=1;
+    public static final int WAGE_PER_HOUR=20;
 
     public static void main(String[] args) {
+        int emphours=0;
+        int empwage=0;
         double empcheck=Math.floor(Math.random()*10)%2;
         if(empcheck==IS_PRESENT)
         {
-            System.out.println("The Employee is Present");
+            emphours=8;
         }
-        else
-        {
-            System.out.println("The Employee is Absent");
-        }
+        empwage=WAGE_PER_HOUR*emphours;
+        System.out.println("Employee Daily wage =" + empwage);
     }
 }
